22737771e93e5f02f8c7249428a7f5aca4886b1f
unknown <ilia.chkoidze12@gmail.com>
2023-05-18 10:25:42 +0400

    add cors and add product server to the listen()

--- src/app.js
+++ src/app.js
@@ -5,14 +5,16 @@ import dotenv from "dotenv";
 import connectToMongo from "./config/mongo.js";
 import jobListRouter from "./routs/joblist-router.js";
 import swaggerMiddleWare from "../middlewares/swagger-middleware.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 connectToMongo();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api", jobListRouter);
 app.use("/", ...swaggerMiddleWare());
 
-app.listen(5555);
+app.listen(process.env.PORT || 5555);
